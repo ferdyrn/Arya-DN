dbdf028bbfce0a65fc5826087ba6725eee782d46
ferdyrn <69664765+ferdyrn@users.noreply.github.com>
2021-02-10 19:31:18 +0700

    Update tutorial.js

--- plugins/tutorial.js
+++ plugins/tutorial.js
@@ -10,9 +10,9 @@ Ketik di *[TERMUX]*
 4) $pkg install nodejs
 
 Untuk Githubnya..
-5) $git clone https://github.com/Arya274/Arya-DN
+5) $git clone https://github.com/ferdyrn/MrF
 6) $ls
-7) $cd Arya-DN
+7) $cd MrF
 
 yang *terakhirr*..
 Penginstalannya cuy :)
